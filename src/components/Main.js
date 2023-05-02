5815314aa90622a89e9874cbab29f7036102f343
Jackeline <89873221+Jacke09@users.noreply.github.com>
2023-05-02 14:03:55 -0300

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Button, Form, Container, Row } from 'react-bootstrap';
+
+import foods from '../models/foods';
+import Food from './Food';
+
+const Main = () => {
+  let [nome, setNome] = useState('');
+
+  const handleClick = (event) => {
+    console.log('Clicou!');
+  };
+
+  const handleChange = (event) => {
+    setNome(event.target.value);
+  };
+
+
+
+  return (
+    <Container>
+      <main>
+        <h1>Menu</h1>
+        {/* Component Button do bootstrap. */}
+        <Form.Group className="mb-3">
+          <Form.Label>Alimento</Form.Label>
+          <Form.Control type="text" placeholder="Café" />
+
+          <input type="text" onChange={handleChange} />
+        </Form.Group>
+        <Button onClick={handleClick} variant="primary">
+          Pesquisar
+        </Button>
+        <Row className="my-2">
+          {foods.map((food) => (
+            <Food food={food}></Food>
+          ))}
+        </Row>
+      </main>
+    </Container>
+  );
+};
+
+export default Main;
